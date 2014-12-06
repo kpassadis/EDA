09980d4101c877985e0d349fc778f358154780a2
kpassadis <kpassadis@yahoo.gr>
2014-12-06 10:32:47 +0200

    Pot3

---
+++ plot3.R
@@ -0,0 +1,15 @@
+powerConsumption<-read.table('~/development/R/EDA/household_power_consumption.txt',header=TRUE,sep=';',stringsAsFactors=FALSE,colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings="?")
+powerConsumption$Date<-as.Date(powerConsumption$Date,format="%d/%m/%Y")
+startDate<-as.Date("2007-02-01")
+endDate<-as.Date("2007-02-02")
+data<-powerConsumption[powerConsumption$Date>=startDate & powerConsumption$Date<=endDate,]
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (KW)",col="red")
+rug(data$Global_active_power)
+
+data$dateTime<-strptime(paste(as.character(data$Date), data$Time, sep=" "),format="%Y-%m-%d %H:%M:%S")
+plot(data$dateTime, data$Global_active_power,type="l",ylab="Global Active Power",xlab="")
+
+plot(data$dateTime, data$Sub_metering_1,type="l",col="red",ylab="Energy sub metering", xlab="")
+lines(data$dateTime, data$Sub_metering_2,col="blue")
+lines(data$dateTime, data$Sub_metering_3,col="green")
+legend('topright', names(data)[7:9] , lty=1, col=c('red', 'blue', 'green',' brown'), bty='n', cex=.75)
